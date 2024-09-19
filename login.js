f02004a4234836a82919d41c40e977787b5ab8cd
NesralKrik <112157345+NesralKrik@users.noreply.github.com>
2024-09-20 07:53:42 +0800

    Update login.js

--- login.js
+++ login.js
@@ -24,7 +24,7 @@ googleLogin.addEventListener("click", function(){
     const credential = GoogleAuthProvider.credentialFromResult(result);
     const user = result.user;
     console.log(user);
-    window.location.href = "main.html"
+    window.location.href = "../main.html"
   }).catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
